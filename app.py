c20595d8df62513401af0a3593de95750b08b3e8
rpmalone44 <129221428+rpmalone44@users.noreply.github.com>
2023-04-28 19:04:43 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+from main import main
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def submit():
+    # get user information
+    name = request.form['name']
+    age = request.form['age']
+    education_level = request.form['education_level']
+    interests = []
+    skills = []
+
+    num_interests = int(request.form['num_interests'])
+    num_skills = int(request.form['num_skills'])
+
+    for i in range(num_interests):
+        interest = request.form[f'interest_{i+1}']
+        interests.append(interest)
+
+    for i in range(num_skills):
+        skill = request.form[f'skill_{i+1}']
+        skills.append(skill)
+
+    # display questions and record answers
+    answers = []
+    for question in main.questions:
+        answers.append(request.form[f'answer_{question["id"]}'])
+
+    # calculate score and give answer
+    score = main.calculate_score(answers)
+    feedback = main.provide_feedback(score)
+
+    return render_template('result.html', name=name, age=age, education_level=education_level, interests=interests, skills=skills, feedback=feedback)
+
+if __name__ == '__main__':
+    app.run()
